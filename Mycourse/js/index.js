d1e61697cd7b7df63b5eb35229aeebff1430ab9e
季新婵 <1209980074@qq.com>
2017-12-26 22:31:56 +0800

    Update index.js

--- Mycourse/js/index.js
+++ Mycourse/js/index.js
@@ -28,17 +28,17 @@ var t,z,p;
 $(document).ready(function(){
 	$('#b1').click(function(){
 		$('#b1').fadeOut(3000);
-		t=setTimeout(function(){window.open('sign.html','_self')},2500);
+		t=setTimeout(function(){window.open('https://htmlpreview.github.io/?https://github.com/jixinchan/web2/blob/master/Mycourse/sign.html','_self')},2500);
 	});
 	$('#b2').click(function(){
 		$('#b2').fadeOut(3000);
-		z=setTimeout(function(){window.open('list.html','_self')},2500);
+		z=setTimeout(function(){window.open('https://htmlpreview.github.io/?https://github.com/jixinchan/web2/blob/master/Mycourse/list.html','_self')},2500);
 	});
 	$('#b3').click(function(){
 		$('#b3').fadeOut(3000);
 		p=setTimeout(function()
 		{
-			window.open('index.html','_self');
+			window.open('https://htmlpreview.github.io/?https://github.com/jixinchan/web2/blob/master/Mycourse/index.html','_self');
 		},2500);
 	});
 })
